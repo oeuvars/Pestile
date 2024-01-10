ea21e24c063602c7bdd4a4b09ba25df4d729e897
Oeuvars <www.anuragniall@gmail.com>
2024-01-10 21:48:46 +0530

    Hero page button image issue

--- src/app/home/hero.tsx
+++ src/app/home/hero.tsx
@@ -11,12 +11,11 @@ const Hero: React.FC = () => {
       </div>
       <div className='phone:w-[90%] lg:w-full mx-auto phone:text-base tablet:text-xl text-[#80796B] text-center phone:mt-[4vh] tablet:mt-[2.5vw] font-satoshi-regular leading-tight'>
         From booking your flights and accommodations to finding local experts and
-
         handling the logistics, your journey is covered from start to finish.
       </div>
       <button className='flex bg-[#F99052] mx-auto px-7 py-2 gap-2 rounded-[9px] shadow-sm hover:shadow-[#FF878C]/40 hover:shadow-xl animation font-satoshi-medium mt-[2.5vw]'>
         <p className='font-satoshi-medium font text-white my-auto text-lg'>Start for free</p>
-        <img src='icons/plane.svg' className='w-7 h-7'/>
+        <img src='/icons/plane.svg' className='w-7 h-7'/>
       </button>
       <hr className='phone:mt-[2.5vh] md:mt-[2.5vw] phone:w-[10%] md:w-[2%] mx-auto border-neutral-300'/>
       <Marquee play={true} gradient gradientColor={[251, 246, 234]} className='phone:my-[5vh] md:my-[2.5vw] -z-10'>
